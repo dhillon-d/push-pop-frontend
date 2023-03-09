d69dcc3b02f7d05f990ea7119571c5ff92d83001
Dilraj Dhillon <dilrajdhillon@gmail.com>
2023-03-09 14:47:46 -0800

    added inputs and outputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import "./App.css";
+import { useState, useRef } from "react";
 
 function App() {
+  const inputRef = useRef(null);
+  const [inputValue, setInputValue] = useState("");
+  const [outputValue, setOutputValue] = useState("");
+  const handleInputClick = () => {
+    setInputValue(inputRef.current.value);
+  };
+  const handleOutputClick = () => {
+    setOutputValue(inputValue);
+  };
+
   return (
     <div className="App">
       <h1>Vite + React</h1>
       <div>
         <label htmlFor="value">value to push: </label>
-        <input type="text" id="value" />
+        <input ref={inputRef} type="text" id="value" name="value" />
+        <p>popped value: {outputValue}</p>
       </div>
       <div>
-        <button>push to stack</button>
-        <button>pop from stack</button>
+        <button onClick={handleInputClick}>push to stack</button>
+        <button onClick={handleOutputClick}>pop from stack</button>
       </div>
     </div>
   );
